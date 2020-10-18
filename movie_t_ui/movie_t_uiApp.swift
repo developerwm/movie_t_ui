8e2330845df07e885ab860579eff87425e3258f3
Taua <root@Tauas-MacBook-Pro.local>
2020-10-18 14:29:20 -0300

    Init project

---
+++ movie_t_ui/movie_t_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_t_uiApp.swift
+//  movie_t_ui
+//
+//  Created by Tauã on 18/10/20.
+//
+
+import SwiftUI
+
+@main
+struct movie_t_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
